40bb6e023f68850b56cbe887bc9d5f5f24aa7554
alice <alice.edn@gmail.com>
2018-07-02 21:22:45 +0200

    switch front end to angular 6

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,38 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CalendarModule } from 'angular-calendar';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import localeFr from '@angular/common/locales/fr';
-import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { LeftMenuComponent } from './left-menu/left-menu.component';
-import { HomeworksComponent } from './homeworks/homeworks.component';
-import { LandingKidsClassComponent } from './landing-kids-class/landing-kids-class.component';
-import { AdminComponent } from './admin/admin.component';
-import { KidsClassComponent } from './landing-kids-class/kids-class/kids-class.component';
-import { CalendarComponent } from './homeworks/calendar/calendar.component';
-
-registerLocaleData(localeFr);
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingKidsClassComponent,
-    LeftMenuComponent,
-    HomeworksComponent,
-    AdminComponent,
-    KidsClassComponent,
-    CalendarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    NgbModalModule.forRoot(),
-    CalendarModule.forRoot()
+    CoreModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SigninComponent } from './signin/signin.component';
+
+const authRoutes: Routes = [
+  { path: 'login', component: SigninComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(authRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule {}
