08662063903a8440b12d1d4ab5d3ae5b35861b72
alice <alice.edn@gmail.com>
2018-10-06 15:18:40 +0200

    move form into a modal

--- angular/src/app/admin/user-admin/create-user/create-user.component.ts
+++ angular/src/app/admin/user-admin/create-user/create-user.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import * as fromStore from '../../store';
 import * as UserAdminActions from '../../store/actions/userAdmin.actions';
@@ -17,54 +19,64 @@ export class CreateUserComponent implements OnInit {
 
   roles$: Observable<Array<RoleDto>>;
   error$: Observable<String>;
-  editMode: boolean;
+  editMode$: Observable<boolean>;
+  userToEdit$: Observable<UserDto>;
   userForm: FormGroup;  
-  userId: number;
+  userIdToEdit: number;
+  modalRef: BsModalRef;
+  @ViewChild('modal') modal; 
 
-  constructor(private store: Store<fromStore.UserAdminState>) { 
-    
+  constructor(private store: Store<fromStore.UserAdminState>, private modalService: BsModalService) {
   }
 
   ngOnInit() {
     this.roles$ = this.store.select(fromStore.getRoles);    
-    this.error$ = this.store.select(fromStore.getError);
-    this.userForm = new FormGroup({
-      'name': new FormControl( '', Validators.required ),
-      'firstname': new FormControl( '', Validators.required ),
-      'email': new FormControl( '', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/)] ),
-      'role': new FormControl( '', Validators.required ),
-      'password': new FormControl( '', Validators.required )
-    });
+    this.error$ = this.store.select(fromStore.getError);   
+    this.editMode$ = this.store.select(fromStore.getEditMode);
+    this.userToEdit$ = this.store.select(fromStore.getUser);
+    this.initForm();
     
-    this.store.select(fromStore.getEditMode).subscribe(res=>{      
-      this.editMode=res;
-      if (this.editMode) this.fillForm();
-    })   
+    this.editMode$.subscribe( res => {
+      if (res) {
+        this.fillForm();
+        this.openModal(this.modal);
+      }
+    })
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  initForm() {
+      this.userForm = new FormGroup({
+        'name': new FormControl( '', Validators.required ),
+        'firstname': new FormControl( '', Validators.required ),
+        'email': new FormControl( '', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/)] ),
+        'role': new FormControl( '', Validators.required ),
+        'password': new FormControl( '', Validators.required )
+      }); 
   }
 
   fillForm() {
-      let user: Observable<UserDto> = this.store.select(fromStore.getUser);
-      user.subscribe(res=> {console.log(res) 
+      
+    this.userToEdit$.subscribe(res=> {
         if (res!== null) {
-        this.userForm.get('firstname').setValue(res.firstname);
-        this.userForm.get('name').setValue(res.name);
-        this.userForm.get('email').setValue(res.email);
-        this.userForm.get('role').setValue(res.roleName);
-        this.userId = res.id;
-        this.userForm.controls['password'].clearValidators();
-        this.userForm.get('password').updateValueAndValidity();  
-        } else {
-          this.userForm.reset();
+          this.userForm.get('firstname').setValue(res.firstname);
+          this.userForm.get('name').setValue(res.name);
+          this.userForm.get('email').setValue(res.email);
+          this.userForm.get('role').setValue(res.roleName);
+          this.userIdToEdit = res.id;
+          this.userForm.controls['password'].clearValidators();
+          this.userForm.get('password').updateValueAndValidity();  
         }
       });    
   }
 
   onSaveUser() {
-
-    let id;   
     
-    let newUser = new UserDto(
-      id,
+    let user = new UserDto(
+      null,
       this.userForm.value['firstname'],
       this.userForm.value['name'],
       this.userForm.value['email'],
@@ -72,17 +84,27 @@ export class CreateUserComponent implements OnInit {
       this.userForm.value['role']
     );
 
-    if ( this.editMode ) {
-      newUser.id = this.userId;
-      this.store.dispatch(new UserAdminActions.SaveUser(newUser));
-    } else this.store.dispatch(new UserAdminActions.CreateUser(newUser));
+    if ( this.isEditModeTrue() ) {
+      user.id = this.userIdToEdit;
+      this.store.dispatch(new UserAdminActions.SaveUser(user));
+    } else this.store.dispatch(new UserAdminActions.CreateUser(user));
+
+    this.userForm.reset();
+    this.modalRef.hide();
     
   }
 
+  isEditModeTrue() {
+    this.editMode$.subscribe( res => {
+      if (res) return true; 
+      else return false;
+    })
+  }
+
   onCancel() {
-    
-    
-    
+    this.modalRef.hide();
+    this.store.dispatch(new UserAdminActions.Clear);
+    this.userForm.reset();   
   }
 
 }
